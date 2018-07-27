aac5ddcfa5375a8e97e076796ab81ac2b0ae1a9d
Giovanni <giosvro@gmail.com>
2018-07-27 20:43:03 -0300

    Create Project

---
+++ LokTarHackingRio/LokTarHackingRio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LokTarHackingRio
+//
+//  Created by Giovanni Severo Barros on 27/07/18.
+//  Copyright © 2018 Giovanni Severo Barros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
