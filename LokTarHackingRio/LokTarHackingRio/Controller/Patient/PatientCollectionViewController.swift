9b931348f352f9574c6a743303340019a787f184
Giovanni <giosvro@gmail.com>
2018-07-28 11:59:05 -0300

    Pushing Project

---
+++ LokTarHackingRio/LokTarHackingRio/Controller/Patient/PatientCollectionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  PatientCollectionViewController.swift
+//  LokTarHackingRio
+//
+//  Created by Giovanni Severo Barros on 28/07/18.
+//  Copyright © 2018 Giovanni Severo Barros. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "CellPatient"
+
+class PatientCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 2
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PatientCollectionViewCell
+        
+        cell.backgroundColor = .red
+        cell.appointmentLabel.text = "alou"
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+}
