feefdb4789abd8cfad479eef351d8efcacb08363
Giovanni <giosvro@gmail.com>
2018-07-28 12:14:11 -0300

    alou
    a

---
+++ LokTarHackingRio/LokTarHackingRio/Model/test.swift
@@ -0,0 +1,9 @@
+//
+//  test.swift
+//  LokTarHackingRio
+//
+//  Created by Giovanni Severo Barros on 28/07/18.
+//  Copyright © 2018 Giovanni Severo Barros. All rights reserved.
+//
+
+import Foundation
